1b7b0dfd6792f58b8690b8a213e5825c55f20a12
Dariel <12119@ulsachihuahua.edu.mx>
2025-02-13 19:30:56 -0600

    Se crea repo

---
+++ mobileProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileProjectsApp.swift
+//  mobileProjects
+//
+//  Created by ITIT2 on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct mobileProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
